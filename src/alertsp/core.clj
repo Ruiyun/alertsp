27d821bffe094b7717695d268bd471eb79de0f87
Ruiyun <ruiyun.wen@gmail.com>
2013-04-23 14:11:31 +0800

    add content-type keyword, and add some unit test for core.

--- src/alertsp/core.clj
+++ src/alertsp/core.clj
@@ -22,14 +22,17 @@
     (when (pos? (.capacity buf))
       (.toString buf StandardCharsets/UTF_8))))
 
-(defn- set-headers! [^HttpMessage message, ^Long cseq, headers]
+(defn- set-headers! [^HttpMessage message, ^Long cseq, content-type headers]
   {:pre [(pos? cseq)]}
-  (-> message (.setHeader RtspHeaders$Names/CSEQ cseq))
-  (doseq [[key val-or-vals] (dissoc headers RtspHeaders$Names/CSEQ RtspHeaders$Names/CONTENT_LENGTH)]
-    (if (string? val-or-vals)
-      (.setHeader message key val-or-vals)
-      (doseq [val val-or-vals]
-        (.addHeader message key val))))
+  (let [headers (-> (reduce #(assoc %1 (-> %2 key name lower-case) (val %2)) {} headers)
+                    (#(if content-type (assoc % "content-type" content-type) %))
+                    (assoc "cseq" (str cseq))
+                    (dissoc "content-length"))]
+    (doseq [[key val-or-vals] headers]
+      (if (string? val-or-vals)
+        (.setHeader message key val-or-vals)
+        (doseq [val val-or-vals]
+          (.addHeader message key val)))))
   message)
 
 (defn- set-body! [^HttpMessage message, body]
@@ -42,9 +45,9 @@
 (defn- get-cseq [^HttpMessage message]
   (-> message (.getHeader RtspHeaders$Names/CSEQ) (Long/parseLong)))
 
-(defn rtsp-map->netty-request [{:keys [method uri cseq headers body]}]
+(defn rtsp-map->netty-request [{:keys [method uri cseq headers content-type body]}]
   (-> (DefaultHttpRequest. RtspVersions/RTSP_1_0 (-> method name upper-case (RtspMethods/valueOf)) uri)
-      (set-headers! cseq headers)
+      (set-headers! cseq content-type headers)
       (set-body! body)))
 
 (defn netty-request->rtsp-map [^HttpRequest request]
@@ -52,15 +55,17 @@
    :uri (.getUri request)
    :cseq (get-cseq request)
    :headers (get-headers request)
+   :content-type (.getHeader request RtspHeaders$Names/CONTENT_TYPE)
    :body (get-body request)})
 
-(defn rtsp-map->netty-response [{:keys [status cseq headers body]}]
+(defn rtsp-map->netty-response [{:keys [status cseq headers content-type body]}]
   (-> (DefaultHttpResponse. RtspVersions/RTSP_1_0 (HttpResponseStatus/valueOf status))
-      (set-headers! cseq headers)
+      (set-headers! cseq content-type headers)
       (set-body! body)))
 
 (defn netty-response->rtsp-map [^HttpResponse response]
   {:status (.. response getStatus getCode)
    :cseq (get-cseq response)
    :headers (get-headers response)
+   :content-type (.getHeader response RtspHeaders$Names/CONTENT_TYPE)
    :body (get-body response)})
--- test/alertsp/core_test.clj
+++ test/alertsp/core_test.clj
@@ -3,7 +3,7 @@
         alertsp.core)
   (:import [java.nio.charset StandardCharsets]
            [org.jboss.netty.handler.codec.http DefaultHttpRequest DefaultHttpResponse]
-           [org.jboss.netty.handler.codec.rtsp RtspVersions RtspMethods RtspHeaders$Names]
+           [org.jboss.netty.handler.codec.rtsp RtspVersions RtspMethods RtspHeaders$Names RtspResponseStatuses]
            [org.jboss.netty.buffer ChannelBuffers]))
 
 (deftest netty-request->rtsp-map-test
@@ -15,6 +15,7 @@
             :uri "rtsp://localhost/"
             :cseq 1
             :headers {"cseq" "1", "user-agent" "alertsp-test"}
+            :content-type nil
             :body nil})))
   (testing "basic test with content"
     (is (= (netty-request->rtsp-map (doto (DefaultHttpRequest. RtspVersions/RTSP_1_0 RtspMethods/DESCRIBE "rtsp://localhost/")
@@ -25,4 +26,83 @@
             :uri "rtsp://localhost/"
             :cseq 2
             :headers {"cseq" "2", "content-type" "text/plain"}
+            :content-type "text/plain"
             :body "Hello World!"}))))
+
+(deftest netty-response->rtsp-map-test
+  (testing "basic test without content"
+    (is (= (netty-response->rtsp-map (doto (DefaultHttpResponse. RtspVersions/RTSP_1_0 RtspResponseStatuses/OK)
+                                       (.setHeader RtspHeaders$Names/CSEQ 1)
+                                       (.setHeader RtspHeaders$Names/SERVER "alertsp-test-server")))
+           {:status 200
+            :cseq 1
+            :headers {"cseq" "1", "server" "alertsp-test-server"}
+            :content-type nil
+            :body nil})))
+  (testing "basic test with content"
+    (let [sdp (str "v=0\r\n"
+                   "o=- 15354532072267698938 15354532072267698938 IN IP4 Kay-PC\r\n"
+                   "c=IN IP4 0.0.0.0\r\n"
+                   "t=0 0\r\n")]
+      (is (= (netty-response->rtsp-map (doto (DefaultHttpResponse. RtspVersions/RTSP_1_0 RtspResponseStatuses/NOT_FOUND)
+                                         (.setHeader RtspHeaders$Names/CSEQ 2)
+                                         (.setHeader RtspHeaders$Names/CONTENT_TYPE "application/sdp")
+                                         (.setContent (ChannelBuffers/copiedBuffer sdp StandardCharsets/UTF_8))))
+             {:status 404
+              :cseq 2
+              :headers {"cseq" "2", "content-type" "application/sdp"}
+              :content-type "application/sdp"
+              :body sdp})))))
+
+(deftest rtsp-map->netty-request-test
+  (testing "basic test without content"
+    (let [result (rtsp-map->netty-request {:method :options
+                                           :uri "rtsp://192.168.1.1/something"
+                                           :cseq 1
+                                           :headers {"host" "192.168.1.2", "user-agent" "alertsp-client"}})]
+      (is (= (.. result getMethod getName) "OPTIONS"))
+      (is (= (.getUri result) "rtsp://192.168.1.1/something"))
+      (is (= (.getHeader result RtspHeaders$Names/CSEQ) "1"))
+      (is (= (.getHeader result RtspHeaders$Names/HOST) "192.168.1.2"))
+      (is (= (.getHeader result RtspHeaders$Names/USER_AGENT) "alertsp-client"))
+      (is (zero? (.getContentLength result)))))
+  (testing "basic test with content"
+    (let [result (rtsp-map->netty-request {:method :play
+                                           :uri "rtsp://192.168.1.1/something"
+                                           :cseq 2
+                                           :headers {"session" "3bda72fe72d73e60"
+                                                     "range" "npt=0.000-"}
+                                           :content-type "text/plain"
+                                           :body "Hello world!"})]
+      (is (= (.. result getMethod getName) "PLAY"))
+      (is (= (.getUri result) "rtsp://192.168.1.1/something"))
+      (is (= (.getHeader result RtspHeaders$Names/CSEQ) "2"))
+      (is (= (.getHeader result RtspHeaders$Names/SESSION) "3bda72fe72d73e60"))
+      (is (= (.getHeader result RtspHeaders$Names/RANGE) "npt=0.000-"))
+      (is (= (.getHeader result RtspHeaders$Names/CONTENT_TYPE) "text/plain"))
+      (is (= (.getContentLength result) 12))
+      (is (= (.. result getContent (toString StandardCharsets/UTF_8)) "Hello world!")))))
+
+(deftest rtsp-map->netty-response-test
+  (testing "basic test without content"
+    (let [result (rtsp-map->netty-response {:status 200
+                                            :cseq 1})]
+      (is (= (.getStatus result) RtspResponseStatuses/OK))
+      (is (= (.getHeader result RtspHeaders$Names/CSEQ) "1"))
+      (is (zero? (.getContentLength result)))))
+  (testing "basic test with content"
+    (let [sdp (str "v=0\r\n"
+                   "o=- 15354532072267698938 15354532072267698938 IN IP4 Kay-PC\r\n"
+                   "c=IN IP4 0.0.0.0\r\n"
+                   "t=0 0\r\n")
+          result (rtsp-map->netty-response {:status 403
+                                            :cseq 2
+                                            :headers {"server" "alertsp-test-server"}
+                                            :content-type "application/sdp"
+                                            :body sdp})]
+      (is (= (.getStatus result) RtspResponseStatuses/FORBIDDEN))
+      (is (= (.getHeader result RtspHeaders$Names/CSEQ) "2"))
+      (is (= (.getHeader result RtspHeaders$Names/SERVER) "alertsp-test-server"))
+      (is (= (.getHeader result RtspHeaders$Names/CONTENT_TYPE) "application/sdp"))
+      (is (= (.getContentLength result) (.length sdp)))
+      (is (= (.. result getContent (toString StandardCharsets/UTF_8)) sdp)))))
