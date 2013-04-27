7f112f9be4d78c386749de664e241e8241194c03
Ruiyun <ruiyun.wen@gmail.com>
2013-04-28 00:52:31 +0800

    use latest aleph

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [io.netty/netty "3.6.5.Final"]
-                 [aleph "0.3.0-beta8"]])
+                 [aleph "0.3.0-beta15"]
+                 [lamina "0.5.0-beta15"]])
